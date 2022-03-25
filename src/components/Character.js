8dec3348ec277d36ae7eee8282d80de5e88abbd3
Sean Marsden <sean@Seans-MacBook-Pro.local>
2022-03-24 19:35:00 -0700

    working on APP.js

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,9 +1,28 @@
 import React from "react";
 import styled from 'styled-components';
 
+const StarStyle = styled.div`
+display:flex;
+justify-content:center`
+
+
+const StarStyleBox = styled.div`
+margin:
+background-color:
+border:
+border-radius:
+`
+
+
 const Character = props => {
-    const {info} =props
+    const {info} = props
     return (
+        <StarStyle>
+            <h1>{info.name}</h1>
+        </StarStyle>
         
     )
-}
+};
+
+
+export default Character;
